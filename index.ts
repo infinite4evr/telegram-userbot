6a8c22ad3d5f8085219a0b312e481ad76bcecfba
infinite4evr <ggs.sudhanshu@gmail.com>
2021-03-10 00:38:53 +0530

    Remove ignored files

--- index.ts
+++ index.ts
@@ -1,5 +1,8 @@
 import dotenv from "dotenv";
 dotenv.config();
+import tags from "./tags.json";
+
+const tagsRegex = new RegExp(tags.tags.join("|"), "i");
 
 import { Airgram, Auth, Message, MessageText, prompt } from "airgram";
 
@@ -9,6 +12,22 @@ const airgram = new Airgram({
   command: process.env.TDLIB_COMMAND,
   logVerbosityLevel: 2,
 });
+
+const percentRegex = /[3-9]\d%/;
+const myChatId = -1001481850800;
+
+const channels = [-1001450712440, -1001156896568, -1001259793382, -504416139];
+
+const forwardMessage = (message: Message) => {
+  airgram.api
+    .forwardMessages({
+      chatId: myChatId,
+      fromChatId: message.chatId,
+      messageIds: [message.id],
+    })
+    .then((s) => console.log(s))
+    .catch((err) => console.log(err));
+};
 
 airgram.use(
   new Auth({
@@ -21,21 +40,14 @@ const isTextMessage = (message: Message) => message.content._ === "messageText";
 
 airgram.on("updateNewMessage", async ({ update }) => {
   const { message } = update;
+  console.log(message);
 
-  if (isTextMessage(message)) {
+  if (channels.includes(message.chatId) && isTextMessage(message)) {
     const textMessage = message.content as MessageText;
-    if (textMessage.text.text.includes("hi")) {
-      airgram.api.sendMessage({
-        chatId: message.chatId,
-        messageThreadId: message.messageThreadId,
-        inputMessageContent: {
-          _: "inputMessageText",
-          text: {
-            _: "formattedText",
-            text: "Hi, Good Morning, Can we talk later please?",
-          },
-        },
-      });
+    const text = textMessage.text.text;
+
+    if (percentRegex.test(text) || tagsRegex.test(text)) {
+      forwardMessage(message);
     }
   }
 });
